6d7ccc9276e02bcfd32c0c670ff770013bc7da38
pablo ruben gross <pablorubengross@gmail.com>
2021-11-18 20:22:51 -0300

    version 0.1.3 inicializacion propia de las clases

--- tp3_pablo_gross_ferico_escobar/src/App.js
+++ tp3_pablo_gross_ferico_escobar/src/App.js
@@ -1,35 +1,30 @@
-import logo from './logo.svg';
-import './App.css'; 
-import Home from './paginas/home';
-import Detalle from './paginas/Detalle';
-import Login from './paginas/login';
-import Registro from './paginas/registro';
-import Navbar from './componentes/Navbar';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./paginas/home";
+import Detalle from "./paginas/Detalle";
+import Login from "./paginas/login";
+import Registro from "./paginas/registro";
+import Navbar from "./componentes/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  
   return (
     <>
-    <Navbar />
-    <Switch>
-      <Route path='/'>
-        <Home />
-      </Route>
-      <Route path='/detalle'>
-        <Detalle />
-      </Route>
-      <Route path='/login'>
-        <Login />
-      </Route>
-      <Route path='/registro'>
-        <Registro />
-      </Route>
-    </Switch>
+      <Navbar />
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/detalle">
+          <Detalle />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/registro">
+          <Registro />
+        </Route>
+      </Routes>
     </>
   );
 }
