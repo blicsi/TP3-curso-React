cd89c0638335ac72b940fa86f29c21b95ca871d7
unknown <federicoesc13@gmail.com>
2021-11-18 21:01:13 -0300

    aosdkamlkwd

--- tp3_pablo_gross_ferico_escobar/src/App.js
+++ tp3_pablo_gross_ferico_escobar/src/App.js
@@ -1,30 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Home from "./paginas/home";
-import Detalle from "./paginas/Detalle";
-import Login from "./paginas/login";
-import Registro from "./paginas/registro";
-import Navbar from "./componentes/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'; 
+import Home from './paginas/Home';
+import Detalle from './paginas/Detalle';
+import Login from './paginas/Login';
+import Registro from './paginas/Registro';
+import Navbar from './componentes/Navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom"
 
 function App() {
+  
   return (
     <>
-      <Navbar />
+    <Navbar />
+      <Router>
       <Routes>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/detalle">
-          <Detalle />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/registro">
-          <Registro />
-        </Route>
+      <Route path='/'>
+        <Home />
+      </Route>
+      <Route path='/detalle'>
+        <Detalle />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='/registro'>
+        <Registro />
+      </Route>
       </Routes>
+      </Router>
     </>
   );
 }
