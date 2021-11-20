62b101fe14619552af2eb4ef0fc91d42b40c2bac
pablo ruben gross <pablorubengross@gmail.com>
2021-11-20 19:26:51 -0300

    0.1.7 pablo config de firebase creada

---
+++ tp3_pablo_gross_ferico_escobar/src/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth"
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdsw1-aLbjWJYpvBkxrEmBCJ3egS4O7u4",
+  authDomain: "neon-rampart-258902.firebaseapp.com",
+  projectId: "neon-rampart-258902",
+  storageBucket: "neon-rampart-258902.appspot.com",
+  messagingSenderId: "1074390870868",
+  appId: "1:1074390870868:web:f19ddaeb84db41ed9e36c1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.db= firebase.firestore();
+firebase.auth=firebase.auth()
+
+export default firebase
--- tp3_pablo_gross_ferico_escobar/src/paginas/registro.js
+++ tp3_pablo_gross_ferico_escobar/src/paginas/registro.js
@@ -1,10 +1,11 @@
 import { TextField, Box, Typography, Button } from "@mui/material";
+import firebase from "../config/firebase";
 const Registro = () => {
     return ( 
     <> 
     <Box style={{boxShadow: "60px teal"}}>
         <div style={{marginTop:"11rem"}}>
-            <Typography align="center" variant="h5" style={{color:"black", marginTop:"1rem" , marginLeft:"1rem"}}> Register </Typography>
+            <Typography  variant="h5" style={{color:"black", marginTop:"1rem" , marginLeft:"40rem"}}> Register </Typography>
             <div style={{marginLeft:"36rem", marginTop:"1rem"}}>
                  <TextField id="outlined-basic" label="Email" variant="outlined" size="small" />
             </div>
