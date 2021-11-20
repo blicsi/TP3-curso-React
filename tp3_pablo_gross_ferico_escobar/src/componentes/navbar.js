9d3ed913957be38bc90d23bd54fafd8d7ccbe4af
pablo ruben gross <pablorubengross@gmail.com>
2021-11-20 15:16:30 -0300

    version 0.1.5 maquetado hecho

--- tp3_pablo_gross_ferico_escobar/src/componentes/navbar.js
+++ tp3_pablo_gross_ferico_escobar/src/componentes/navbar.js
@@ -1,8 +1,41 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+    
+    function handleButtononClicl(){
+        
+    }
+    
     return ( 
-        <> 
+        <>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" >
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="secondary"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Carniceria "Bos Taurus"
+            </Typography>
+            <Button color="inherit" onClick= {() => handleButtonClick} to="/">Home</Button>
+            <Button color="inherit" onClick={Link} to="/login">Login</Button>
+            <Button color="inherit">Registro</Button>
+            </Toolbar>
+            </AppBar>
+        </Box>
         </>
      );
 }
- 
 export default Navbar;
---
+++ tp3_pablo_gross_ferico_escobar/src/paginas/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (  
+        <>
+        <h1 style={{textAlign:"center", fontSize:"100px"}}>ERROR 404</h1>
+        <h1 style={{textAlign:"center", fontSize:"100px"}}>PAGE NOT FOUND</h1>
+        </>
+    );
+}
+ 
+export default NotFound;
