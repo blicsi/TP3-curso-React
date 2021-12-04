be86a45dcd35d2ebd49d6228b09772fe78c25b61
pablo ruben gross <pablorubengross@gmail.com>
2021-12-04 15:08:18 -0300

    version 0.2.5

--- tp3_pablo_gross_ferico_escobar/src/App.js
+++ tp3_pablo_gross_ferico_escobar/src/App.js
@@ -5,7 +5,7 @@ import Detalle from "./paginas/Detalle";
 import Login from "./paginas/Login";
 import Registro from "./paginas/Registro";
 import Navbar from "./componentes/Navbar";
-import NotFound from "./paginas/NotFound"
+import NotFound from "./paginas/NotFound";
 import {
   BrowserRouter as Router,
   Routes,
